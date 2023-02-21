e06e6eb9f4ba3fc2163b412b0a237fff3cf2936a
KonDik666 <system518err@gmail.com>
2023-02-21 12:11:39 +0500

    Добавьте файлы проекта.

---
+++ SNILS/SNILS_CLASS_TEST.cs
@@ -0,0 +1,328 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SNILS_LIBRARY;
+
+namespace SNILS
+{
+    [TestClass]
+    public class SNILS_CLASS_TEST
+    {
+        [TestMethod]
+        public void Null_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = " ";
+            bool expected = false;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Uncorect_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "112-233-445 95";
+            bool expected = false;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Uncorect_Limit_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "333";
+            bool expected = false;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Uncorect_Limit_Max_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "999999999999999999999";
+            bool expected = false;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct01_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "15795916329";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void Correct02_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "20401330880";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct03_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "17611432955";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct04_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "19160771478";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct05_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "16570395587";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct06_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "17838689649";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct07_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "16992402305";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct08_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "20398397186";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct09_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "19978255360";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct10_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "16067055052";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct11_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "20043715906";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct12_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "20874913181";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct13_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "15955163599";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Correct14_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "19596091835";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Correct15_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "16454064666";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Correct16_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "15974550714";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Correct17_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "20208120183";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Correct18_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "20149847051";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Correct19_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "17460248064";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Correct20_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "20057830427";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Correct21_Text_CheckPersonakCode()
+        {
+            //Arange
+            string textString = "20508756552";
+            bool expected = true;
+            //Act
+            SNILS_CLASS sn = new SNILS_CLASS();
+            bool actual = sn.CheckPersonakCode(textString);
+            //Assets
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ SNILS_LIBRARY/SNILS_CLASS.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNILS_LIBRARY
+{
+    public class SNILS_CLASS
+    {
+        public bool CheckPersonakCode(string textString)
+        {
+            if (string.IsNullOrEmpty(textString))
+            {
+                return false;
+            }
+            foreach (char c in textString)
+            {
+                if (!char.IsDigit(c)) 
+                {
+                    return false;
+                };
+            }
+            if (textString.Length != 11)
+            {
+                return false;
+            } 
+            int[] mas = new int[11];
+            for (int i = 0; i < mas.Length-2;i++)
+            {
+                mas[i] = Convert.ToInt32(Char.GetNumericValue(textString[i]));
+            }
+            int sum = Convert.ToInt32(textString.Substring(9,2));
+            int[] pos = new int[9] { mas[0] * 9, mas[1] * 8, mas[2] * 7, mas[3] * 6, mas[4] * 5, mas[5] * 4, mas[6] * 3, mas[7] * 2, mas[8] * 1, };
+            int possum = 0;
+            for (int i =0; i < pos.Length;i++)
+            {
+                possum+= pos[i];
+            }
+            if (possum > 101)
+            {
+                possum = possum % 101;
+            }
+            int asum = 0;
+            if (sum == 0)
+            {
+                sum = 100;
+                asum = 101;
+            }
+            if (possum > 101)
+            {
+                possum = possum % 101;
+            }
+            if (possum == sum || possum == asum)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
